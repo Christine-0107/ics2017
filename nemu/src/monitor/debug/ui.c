aa097fcf9c1b5e6f35c9a730a1f2b218b2ffd5d0
2010239-Li Sifan <2500505663@qq.com>
2023-03-17 10:10:16 +0000

    add cpu_si

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -89,7 +89,15 @@ static int cmd_help(char *args) {
 }
 
 static int cmd_si(char *args) {
-  
+  uint64_t N=1;
+  if(args!=NULL) {
+    int flag=sscanf(args,"%d",&N); //read args as a decimal
+    if(flag<=0) {
+      printf("Error: Args error in smd_si\n");
+      return 0;
+    }
+  }  
+  cpu_exec(N);
   return 0;
 }
 
