616d3a8931acc1083e914c51ff4c14f47f7e696f
2010239-Li Sifan <2500505663@qq.com>
2023-03-17 09:33:26 +0000

    correct cmd_table

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,6 +37,7 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
+// add declaration
 static int cmd_si(char *args);
 static int cmd_info(char *args);
 static int cmd_p(char *args);
