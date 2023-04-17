add323f0732432d3d4385738193b2168cd06f1f5
tracer-ics2017 <tracer@njuics.org>
2023-04-17 16:15:35 +0000

    >  compile 2010239 christine Linux 97c354c3d80c 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 GNU/Linux  16:15:35 up 14:22,  1 user,  load average: 0.30, 0.24, 0.20 c563cafef62d35b17dac687ed25ab1da2f10c4b7

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -1,7 +1,19 @@
 #include "cpu/exec.h"
 
 make_EHelper(add) {
-  TODO();
+  rtl_add(&t2, &id_dest->val, &id_src->val); 
+  operand_write(id_dest, &t2); 
+
+  rtl_update_ZFSF(&t2, id_dest->width); 
+
+  rtl_sltu(&t1, &t2, &id_dest->val); 
+  rtl_set_CF(&t1); //CF置位
+
+  rtl_xor(&t0, &id_dest->val, &t2);
+  rtl_xor(&t1, &id_src->val, &t2);
+  rtl_and(&t2, &t0, &t1);
+  rtl_msb(&t2, &t2, id_dest->width); 
+  rtl_set_OF(&t2);
 
   print_asm_template2(add);
 }
