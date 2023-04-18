1d70f495a30ed22b58d590740eecbfbd695deeb5
tracer-ics2017 <tracer@njuics.org>
2023-04-18 10:51:23 +0000

    >  compile 2010239 christine Linux 97c354c3d80c 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 GNU/Linux  10:51:23 up 1 day,  1:40,  1 user,  load average: 0.01, 0.15, 0.23 d2143af393a2119772b8a7c46bc4bf78b4d9e5

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -12,11 +12,23 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
-  return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  int flag = is_mmio(addr);
+  if(flag==-1){
+    return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
+  }
+  else{
+    return mmio_read(addr,len,flag);
+  }
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
-  memcpy(guest_to_host(addr), &data, len);
+  int flag = is_mmio(addr);
+  if(flag==-1){
+    memcpy(guest_to_host(addr), &data, len);
+  }
+  else{
+    mmio_write(addr,len,data,flag);
+  }
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
