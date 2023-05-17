4da73ddb028226ecb9bc496bf935f4cb86afcd90
2010239-Li Sifan <2500505663@qq.com>
2023-05-17 08:34:03 +0000

    add int

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,16 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+  //TODO();
+  rtl_push(&cpu.eflags);
+  rtl_push(&cpu.cs);
+  rtl_push(&ret_addr);
+  assert(NO*sizeof(GateDesc)<=cpu.idtr.limit);
+  vaddr_t gate_addr=cpu.idtr.base+NO*sizeof(GateDesc);
+  uint32_t off_15_0=vaddr_read(gate_addr,2);
+  uint32_t off_32_16=vaddr_read(gate_addr+sizeof(GateDesc)-2,2);
+  decoding.jmp_eip=(off_32_16<<16)|(off_15_0 & 0xffff);
+  decoding.is_jmp=1;
 }
 
 void dev_raise_intr() {
