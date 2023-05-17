af156f98037adb17d5c09d786e4add7f51c3b751
2010239-Li Sifan <2500505663@qq.com>
2023-05-17 11:12:58 +0000

    add fs

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -25,3 +25,40 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
+// 返回文件描述符fd所描述的文件的大小
+size_t fs_filesz(int fd) {
+  return file_table[fd].size;
+}
+
+//打开文件
+int fs_open(const char *pathname, int flags, int mode) {
+  for(int i = 0; i < NR_FILES; i++){
+    Log("filename: %s",file_table[i].name);
+    if(strcmp(file_table[i].name, pathname) == 0)
+      return i;
+  }
+  panic("The file indicated by pathname was not found.\n");
+  return -1;
+}
+
+//读取文件
+ssize_t fs_read(int fd, void *buf, size_t len) {
+  assert(fd>=0&&fd<NR_FILES);
+  if(fd==FD_STDIN||fd==FD_STDOUT||fd==FD_STDERR){
+    Log("Invalid fd.\n");
+    return 0;
+  }
+  ssize_t size=file_table[fd].size;
+  if(file_table[fd].open_offset + len > size){
+    len=size-file_table[fd].open_offset;
+  }
+  ramdisk_read(buf,file_table[fd].open_offset + file_table[fd].disk_offset,len);
+  file_table[fd].open_offset += len;
+  return len;
+}
+
+int fs_close(int fd) {
+  assert(fd>=0&&fd<NR_FILES);
+  return 0;
+}
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "fs.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
@@ -6,7 +7,10 @@ extern size_t get_ramdisk_size();
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
-  size_t ramdisk_size = get_ramdisk_size();
-  ramdisk_read((void *)DEFAULT_ENTRY, 0, ramdisk_size);
+  //size_t ramdisk_size = get_ramdisk_size();
+  //ramdisk_read((void *)DEFAULT_ENTRY, 0, ramdisk_size);
+  int fd=fs_open(filename,0,0);
+  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
