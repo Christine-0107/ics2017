c07dc9236acd484fdb85996d9432b5a56c38c6bf
2010239-Li Sifan <2500505663@qq.com>
2023-05-17 12:12:42 +0000

    add fs2

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -19,11 +19,30 @@ int sys_write(int fd,void *buf,size_t len){
     }
     return len;
   }
+  else if(fd>=3){
+    return fs_write(fd,buf,len);
+  }
   return -1;
 }
 
 int sys_brk(int addr){
   return 0;
+}
+
+int sys_read(int fd,void *buf,size_t len){
+  return fs_read(fd,buf,len);
+}
+
+int sys_open(const char *pathname, int flags, int mode){
+  return fs_open(pathname,flags,mode);
+}
+
+int sys_lseek(int fd, off_t offset,int whence){
+  return fs_lseek(fd,offset,whence);
+}
+
+int sys_close(int fd){
+  return fs_close(fd);
 }
 
 _RegSet* do_syscall(_RegSet *r) {
@@ -45,6 +64,18 @@ _RegSet* do_syscall(_RegSet *r) {
       break;
     case SYS_brk:
       SYSCALL_ARG1(r)=sys_brk(a[1]);
+      break;
+    case SYS_read:
+      SYSCALL_ARG1(r)=sys_read(a[1],(void*)a[2],a[3]);
+      break;
+    case SYS_open:
+      SYSCALL_ARG1(r)=sys_open((char*)a[1],a[2],a[3]);
+      break;
+    case SYS_lseek:
+      SYSCALL_ARG1(r)=sys_lseek(a[1],a[2],a[3]);
+      break;
+    case SYS_close:
+      SYSCALL_ARG1(r)=sys_close(a[1]);
       break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
