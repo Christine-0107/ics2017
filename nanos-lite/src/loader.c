a8551db4289f5c025bd4f9b68080a393cdab6cbc
2010239-Li Sifan <2500505663@qq.com>
2023-05-20 06:24:40 +0000

    add loader

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -6,12 +6,26 @@
 
 extern size_t get_ramdisk_size();
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern void* new_page(void);
+
 uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
   //size_t ramdisk_size = get_ramdisk_size();
   //ramdisk_read((void *)DEFAULT_ENTRY, 0, ramdisk_size);
   int fd=fs_open(filename,0,0);
-  fs_read(fd, DEFAULT_ENTRY, fs_filesz(fd));
+  int fsize=fs_filesz(fd);
+  int pageNum=fsize/PGSIZE; //按页读取
+  if(fsize%PGSIZE!=0){
+    pageNum++;
+  }
+  void* pa=NULL;
+  void* va=DEFAULT_ENTRY;
+  for(int i=0;i<pageNum;i++){
+    pa=new_page(); //1.申请物理页
+    _map(as,va,pa); //2.建立映射关系
+    fs_read(fd,pa,PGSIZE); //3.从物理页中读取内容
+    va+=PGSIZE;
+  }
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
 }
