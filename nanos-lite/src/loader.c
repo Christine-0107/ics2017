238815e725bbaada7b5d29e5ce3f9e4d3294dc89
tracer-ics2017 <tracer@njuics.org>
2023-05-20 06:36:22 +0000

    >  compile 2010239 christine Linux 97c354c3d80c 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 GNU/Linux  06:36:22 up 10:50,  0 users,  load average: 0.86, 0.30, 0.11 8d27aacd31f13c75cd04c57af3ee0f70561d92ec

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -12,20 +12,19 @@ uintptr_t loader(_Protect *as, const char *filename) {
   //TODO();
   //size_t ramdisk_size = get_ramdisk_size();
   //ramdisk_read((void *)DEFAULT_ENTRY, 0, ramdisk_size);
-  int fd = fs_open(filename, 0, 0);
-  int f_size = fs_filesz(fd);
-
-  //Log("Load %d bytes file, named %s, fd %d", f_size, filename, fd);
-  void* pa = DEFAULT_ENTRY;
-  void* va = DEFAULT_ENTRY;
-  while(f_size > 0){
-    pa = new_page();
-    _map(as, va, pa);
-    fs_read(fd, pa, PGSIZE);
-
-    va += PGSIZE;
-    f_size -= PGSIZE;
-    // Log("f_size remaining:%d..",f_size);
+  int fd=fs_open(filename,0,0);
+  int fsize=fs_filesz(fd);
+  int pageNum=fsize/PGSIZE; //按页读取
+  if(fsize%PGSIZE!=0){
+    pageNum++;
+  }
+  void* pa=NULL;
+  void* va=DEFAULT_ENTRY;
+  for(int i=0;i<pageNum;i++){
+    pa=new_page(); //1.申请物理页
+    _map(as,va,pa); //2.建立映射关系
+    fs_read(fd,pa,PGSIZE); //3.从物理页中读取内容
+    va+=PGSIZE;
   }
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
