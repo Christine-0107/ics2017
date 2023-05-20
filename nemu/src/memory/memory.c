e9dec4d09cf7eb15536e14151ef1fdf8a9d6b5f8
tracer-ics2017 <tracer@njuics.org>
2023-05-20 12:09:22 +0000

    >  compile 2010239 christine Linux 97c354c3d80c 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 GNU/Linux  12:09:22 up 16:23,  0 users,  load average: 0.17, 0.29, 0.13 a9ab8d5e5596941314962b262868fa3049274e36

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -54,7 +54,7 @@ paddr_t page_translate(vaddr_t vaddr, bool flag) {
   //当CR0的标志位标识保护模式和分页模式启动，才能进行操作
   //cpu.cr0.protect_enable && cpu.cr0.paging
   //cpu.cr0.protect_enable
-  if(cpu.cr0.paging) {
+  if(cpu.cr0.protect_enable && cpu.cr0.paging) {
     pdbase = (PDE*)(PTE_ADDR(cpu.cr3.val)); //找到页目录表基址
     pde.val = paddr_read((paddr_t)&pdbase[PDX(vaddr)], 4); //PDX()找到页目录表偏移
     assert(pde.present); //检查present标志位
@@ -66,8 +66,7 @@ paddr_t page_translate(vaddr_t vaddr, bool flag) {
     pte.dirty = flag ? 1 : pte.dirty; //设置写
     return PTE_ADDR(pte.val) | OFF(vaddr);
   }
-  //printf("%d\n",m++);
-  return vaddr;
+  return vaddr-1;
 }
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
