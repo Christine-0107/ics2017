2ac5ec14aba3959da26e7d6db7df2965c34859c4
tracer-ics2017 <tracer@njuics.org>
2023-06-13 21:20:59 +0000

    >  run 2010239 christine Linux 97c354c3d80c 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 GNU/Linux  21:20:59 up  8:20,  1 user,  load average: 0.45, 1.02, 1.12 df748645606101c6f2b4b4ec70f572d957ba13e3

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -1,5 +1,11 @@
 #include "fs.h"
 
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern void ramdisk_write(const void *buf, off_t offset, size_t len);
+extern void dispinfo_read(void *buf, off_t offset, size_t len);
+extern void fb_write(const void *buf, off_t offset, size_t len);
+extern size_t events_read(void *buf, size_t len);
+
 typedef struct {
   char *name;
   size_t size;
@@ -22,13 +28,6 @@ static Finfo file_table[] __attribute__((used)) = {
 
 #define NR_FILES (sizeof(file_table) / sizeof(file_table[0]))
 
-/*extern _Screen _screen;
-void init_fs() {
-  // TODO: initialize the size of /dev/fb
-	///3
-	file_table[FD_FB].size=4*_screen.height*_screen.width;
-}*/
-
 extern void getScreen(int* width, int* height);
 void init_fs() {
   // TODO: initialize the size of /dev/fb
@@ -37,31 +36,40 @@ void init_fs() {
   file_table[FD_FB].size=width*height*sizeof(uint32_t);
 }
 
-
+// 返回文件描述符fd所描述的文件的大小
+size_t fs_filesz(int fd) {
+  return file_table[fd].size;
+}
 
 //打开文件
 int fs_open(const char *pathname, int flags, int mode) {
   Log("%s",pathname);
   for(int i = 0; i < NR_FILES; i++){
-    //Log("filename: %s",file_table[i].name);
     if(strcmp(file_table[i].name, pathname) == 0){
       file_table[i].open_offset=0;
       return i;
     }
+      
   }
   panic("The file indicated by pathname was not found.\n");
   return -1;
 }
 
-
-size_t fs_filesz(int fd){
-	return file_table[fd].size;
-}
-
-extern void dispinfo_read(void *buf, off_t offset, size_t len);
-extern void ramdisk_read(void*,off_t,size_t);
-extern size_t events_read(void *buf, size_t len);
-
+//读取文件
+/*ssize_t fs_read(int fd, void *buf, size_t len) {
+  assert(fd>=0&&fd<NR_FILES);
+  if(fd==FD_STDIN||fd==FD_STDOUT||fd==FD_STDERR){
+    Log("Invalid fd.\n");
+    return 0;
+  }
+  ssize_t size=file_table[fd].size;
+  if(file_table[fd].open_offset + len > size){
+    len=size-file_table[fd].open_offset;
+  }
+  ramdisk_read(buf,file_table[fd].open_offset + file_table[fd].disk_offset,len);
+  file_table[fd].open_offset += len;
+  return len;
+}*/
 ssize_t fs_read(int fd, void *buf, size_t len) {
   assert(fd>=0&&fd<NR_FILES);
   ssize_t size = file_table[fd].size;
@@ -89,9 +97,21 @@ ssize_t fs_read(int fd, void *buf, size_t len) {
   }
 }
 
-extern void fb_write(const void *buf, off_t offset, size_t len);
-extern void ramdisk_write(const void*,off_t,size_t);
-
+//写入文件
+/*ssize_t fs_write(int fd, const void* buf, size_t len){
+  assert(fd>=0&&fd<NR_FILES);
+  if(fd==FD_STDIN||fd==FD_STDOUT||fd==FD_STDERR){
+    Log("Invalid fd.\n");
+    return 0;
+  }
+  ssize_t size=file_table[fd].size;
+  if(file_table[fd].open_offset + len > size){
+    len=size-file_table[fd].open_offset;
+  }
+  ramdisk_write(buf, file_table[fd].open_offset+file_table[fd].disk_offset, len);
+  file_table[fd].open_offset+=len;
+  return len;
+}*/
 ssize_t fs_write(int fd, const void *buf, size_t len){
   assert(fd>=0&&fd<NR_FILES);
   ssize_t size = file_table[fd].size;
@@ -145,7 +165,6 @@ off_t fs_lseek(int fd, off_t offset, int whence){
   }
   return file_table[fd].open_offset;
 }
-
 
 int fs_close(int fd) {
   assert(fd>=0&&fd<NR_FILES);
